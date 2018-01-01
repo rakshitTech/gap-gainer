febd25d9650d5fb81674d82e22c1237bdf958d5f
rakshit <rakshit.yadav@tookanapp.com>
2018-01-01 20:26:26 +0530

    added postgreSQL

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const pg = require("pg")
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
@@ -9,5 +10,16 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => res.send("cool"))
+app.get('/db', function (request, response) {
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+        client.query('SELECT * FROM test_table', function(err, result) {
+            done();
+            if (err)
+            { console.error(err); response.send("Error " + err); }
+            else
+            { response.render('pages/db', {results: result.rows} ); }
+        });
+    });
+});
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
