96aa4156206a58f944f90eaf9915d4a78710fa01
rakshit <rakshit.yadav@tookanapp.com>
2018-01-01 19:44:54 +0530

    added index js

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => res.send("cool"))
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
