0eb704989358ba71ad25afbf5ff2fa22370ffbfc
rakshit <rakshit.yadav@tookanapp.com>
2018-01-03 12:37:08 +0530

    replaced request with needle

--- routes/cryptocurrency.js
+++ routes/cryptocurrency.js
@@ -1,4 +1,4 @@
-var request = require("request");
+var needle = require("needle");
 // var moment = require("moment");
 
 var constants = require("./constants");
@@ -6,16 +6,13 @@ var constants = require("./constants");
 exports.getCurrentRate = getCurrentRate;
 
 function getCurrentRate (req, res) {
-    request(constants.externalFixedApis.koinex_ticker, function (error, response, body) {
-        if (error) {
-            console.log("koinex error" + error);
-            return res.send("error");
-        }
-        console.log("koinex body" + body);
-        if (!body) {
-            console.log("koinex response" + response);
-        }
-
-        return res.send("done");
-    })
+    needle("get", constants.externalFixedApis.koinex_ticker).
+        then(resp => {
+            console.log("koinex body " + resp.body);
+            return res.send("done")
+        }).
+        catch(error => {
+            console.log("koinex error " + error);
+            return res.send("error")
+        })
 }
